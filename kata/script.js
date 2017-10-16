789c1a9793212bd03fadd4a1d1b01a48431b7b5a
pjorda96 <pjorda96@gmail.com>
2017-10-16 13:46:30 +0200

    awserg

---
+++ kata/script.js
@@ -0,0 +1,28 @@
+var pantalla=prompt("Introduce un número de dos cifras");
+var decenas="";
+var unidades="";
+
+if (pantalla<=16 && pantalla>=11)
+  if (pantalla==11){
+    console.log("Once")
+  }else if (pantalla==12){
+    console.log("Doce")
+  }else if (pantalla==13){
+    console.log("Trece")
+  }else if (pantalla==14){
+    console.log("Catorce")
+  }else if (pantalla==15){
+    console.log("Quince")
+}else if (pantalla%10=0){
+  if (pantalla/10=1){console.log("Diez");}
+  else if()
+}else {
+  if (pantalla/10=1){decenas="diez y ";}
+}
+
+
+
+
+
+  if (pantalla==0){unidades}
+}
