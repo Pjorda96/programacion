c02948c6927456042f7a649f419ad39651cc6413
pjorda96 <pjorda96@gmail.com>
2017-11-09 10:44:50 +0100

    adivinar

--- ahorcado/js/script03.js
+++ ahorcado/js/script03.js
@@ -9,53 +9,60 @@ console.log(adivinar);
 
 //contador
 var intentos=5;
-var cont=1;
+var cont=0;
+
+//array de la palabra a palabraAcertar
+var palabraAcertar=[];
+for (let i=0; i<=palabraAcertar.length-1;i++){
+  palabraAcertar[i]=adivinar.charAt(i);
+}
 
 //array de la palabra
 var palabra=[];
-for (let i=0; i<=palabra.length-1;i++){
+for (let i=0; i<=palabraAcertar.length-1;i++){
   palabra[i]="_";
 }
-var preguntar=true;
+var barrabaja=6;
 var comp=false;
 
-while(intentos>cont && preguntar==true){
+while(intentos>cont && barrabaja!=0){
 //pedir letra y comprobar si letra
-  var letra=prompt("Introduce una letra");
+  var letra=prompt("Te quedan "+eval("intentos-cont")+" intentos");;
   while (isNaN(letra)==false){
     letra=prompt("Introduce una letra");
   }
 
   //colocar letra en plantilla
   for (let i=0; i<=palabra.length-1; i++){
-    if (letra == adivinar.charAt(i)) {
+    if (letra == palabraAcertar[i]) {
       palabra[i]=letra;
       comp=true;
     }
   }
 
   //comprobar si encontrada
-  if (comp!=true){
+  if (comp==true){
     console.log(letra);
     alert("Letra encontrada");
   }else{
     cont++
-    alert("Te quedan "+eval("intentos-cont")+" intentos");
   }
   comp=false
 
 //mostrar array
-  let j="";
+  var j="";
   for (let i=0; i<=adivinar.length-1; i++){
     j=j+palabra[i];
+    //console.log(palabra[i]);
   }
   console.log(j);
 
   //comprobar si se ha acertado
   preguntar=false;
   for (let i=0; i<=adivinar.length-1; i++){
-    if(palabra[i]=="_") preguntar=true;
+    if(palabra[i]=="_") barrabaja=barrabaja+1;
   }
 }
 
-alert("¡¡ENHORABUENA!!");
+if (barrabaja==0) alert("¡¡ENHORABUENA!!");
+else alert("Mejor suerte la próxima vez");
