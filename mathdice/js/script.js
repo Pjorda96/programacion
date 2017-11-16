46e54f2584f25436b3833259b90bc8b6aa0b8af1
pjorda96 <pjorda96@gmail.com>
2017-11-16 10:45:55 +0100

    seguir

--- mathdice/js/script.js
+++ mathdice/js/script.js
@@ -1,2 +1,6 @@
-var num=prompt("Introduce un número", "0");
-num=
+var num=prompt("Introduce un número del 1 al 12");
+var dado1=prompt("Introduce un número del 1 al 6");
+var dado2=prompt("Introduce un segundo del 1 al 6");
+var dado3=prompt("Introduce el último del 1 al 6");
+var dado4=prompt("Introduce un numero del 1 al 3");
+var dado5=prompt("Introduce el segundo del 1 al 3");
