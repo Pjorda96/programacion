2306a7db597ced1e5387d309b95b1c36fdea813b
pjorda96 <pjorda96@gmail.com>
2017-11-17 13:29:33 +0100

    bibu

---
+++ ahorcado/js/dubujo.js
@@ -0,0 +1,59 @@
+var dubujo=0;
+var lineas=[];
+
+
+switch(dubujo){
+  case 1:
+  lineas[9]="_ ____    ";
+  break;
+
+  case 2:
+  lineas[2]=" |        ";
+  lineas[3]=" |        ";
+  lineas[4]=" |        ";
+  lineas[5]=" |        ";
+  lineas[6]=" |        ";
+  lineas[7]=" |        ";
+  lineas[8]=" |        ";
+  lineas[9]="_|____    ";
+  break;
+
+  case 3:
+  lineas[1]=" ________ ";
+  lineas[2]=" |        ";
+  lineas[3]=" |        ";
+  lineas[4]=" |        ";
+  lineas[5]=" |        ";
+  lineas[6]=" |        ";
+  lineas[7]=" |        ";
+  lineas[8]=" |        ";
+  lineas[9]="_|____    ";
+  break;
+
+  case 4:
+  lineas[2]=" |      | ";
+  lineas[3]=" |      O ";
+  break;
+
+  case 5:
+  lineas[4]=" |      | ";
+  break;
+
+  case 6:
+  lineas[4]=" |     /| ";
+  break;
+
+  case 7:
+  lineas[4]=" |     /|\ ";
+  break;
+
+  case 8:
+  lineas[5]=" |     /  ";
+  break;
+
+  case 9:
+  lineas[5]=" |     / \ ";
+  break;
+
+  default:
+}
