9b37aa5dccd7df4dd108bf8a3ab70a21dfe771dd
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2017-12-11 13:00:59 +0100

    fghjkl

---
+++ refuerzo/bingoC.js
@@ -0,0 +1,13 @@
+var fin = false;
+var salidos = [];
+while (fin==false){
+  var introduce = prompt("Pulsa ACEPTAR para pedir bola o escribe 'FIN' para parar.");
+  if (introduce=="FIN" || introduce=="Fin" || introduce=="fin") fin=true;
+  else {
+    while (){
+      var numAl= Math.floor((Math.random()*(90-1))+1);
+    }
+    salidos.push[numAl];
+    console.log(numAl);
+  }
+}
