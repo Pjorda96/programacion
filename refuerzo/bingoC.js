8fd2b415212d3d058bcacd2a965c3657e5b4dd0e
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2017-12-12 12:58:25 +0100

    zfdb

--- refuerzo/bingoC.js
+++ refuerzo/bingoC.js
@@ -1,13 +1,28 @@
 var fin = false;
 var salidos = [];
+var usado = false;
+var numAl= Math.floor((Math.random()*(90-1))+1);
 while (fin==false){
   var introduce = prompt("Pulsa ACEPTAR para pedir bola o escribe 'FIN' para parar.");
-  if (introduce=="FIN" || introduce=="Fin" || introduce=="fin") fin=true;
-  else {
-    while (){
-      var numAl= Math.floor((Math.random()*(90-1))+1);
-    }
-    salidos.push[numAl];
-    console.log(numAl);
+  if (introduce=="FIN" || introduce=="Fin" || introduce=="fin") {
+    fin=true;
+    alert("Finalizado");
+  } else {
+      if(salidos.length-1==90) introduce="fin";
+      else if (salidos.length==0){
+        console.log(numAl);
+        salidos.push[numAl];
+      }
+      else {
+        do{
+          numAl= Math.floor((Math.random()*(90-1))+1);
+          for (let i=0; i<salidos.length-1; i++){
+            if(salidos[i]==numAl) usado = true;
+          }
+        } while (usado=false);
+        usado = false;
+        console.log(numAl);
+        salidos.push[numAl];
+      }
   }
 }
