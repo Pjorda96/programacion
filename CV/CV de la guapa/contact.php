71b23fb2212801173bd9d402c13b8e6a05950ee9
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2017-12-17 18:47:05 +0100

    fin

--- CV/CV de la guapa/contact.php
+++ CV/CV de la guapa/contact.php
@@ -2,7 +2,8 @@
 error_reporting(0);
 $nombre = $_POST['nombre'];
 $mail= $_POST['email'];
-$mensaje=$_POST['mensaje'];
+$telefono=$_POST['telefono'];
+$mensaje=$_POST['text'];
 $header = 'From: ' . $mail . ", de la poblacion ".$poblacion."\r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
@@ -10,7 +11,8 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje" . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Su teléfono es: " . $telefono . " \r\n";
+$mensaje .= "Mensaje" . $_POST['text'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = pjorda96@gmail.com;
