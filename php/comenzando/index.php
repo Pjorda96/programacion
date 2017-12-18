c33f8e23ced744ce275f4283f58f56fb596fa7d8
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2017-12-18 13:43:58 +0100

    ghj

---
+++ php/comenzando/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Amunt Valencia</title>
+  </head>
+  <body>
+    <?php
+      //variables
+      $equipo="Valencia";
+      $Equipo="Madrid";
+      $posicionFinalenLiga=1;
+      $ganadorLiga=true;
+    ?>
+    <h1>AMUNT <?php echo $equipo?></h1>
+    <?php echo "<b>Soy blanc-i-negre</b>";?></br>
+    <?php echo $ganadorLiga;?>
+    <?php echo var_dump($ganadorLiga);?>
+  </body>
+</html>
