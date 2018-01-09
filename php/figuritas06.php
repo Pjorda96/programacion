615b100836ebe7a4196261267885843332cd7ac1
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-01-09 13:41:38 +0100

    a mitad figuritas

--- php/figuritas06.php
+++ php/figuritas06.php
@@ -5,6 +5,60 @@
     <title>Figuritas</title>
   </head>
   <body>
+    <div class="fig1">
+      <h4>Figura 1: Muy muy sencilla</h4>
+      <?php
+      $i=1;
+      $j=1;
+      while ($i <= 5) {
+        echo "*"."<br>";
+        $i++;
+      }
+      ?>
+    </div>
+    <div class="fig2">
+      <h4>Figura 2: Sencilla</h4>
+      <?php
+      for ($i=1; $i <= 5; $i++) {
+        for ($j=1; $j <= 5; $j++) {
+          echo "*";
+        }
+        echo "<br>";
+      }
+      ?>
+    </div>
+    <div class="fig3">
+      <h4>Figura 3: Sencilla</h4>
+      <?php
+      for ($i=1; $i <= 7; $i++) {
+        for ($j=1; $j <= $i; $j++) {
+          echo "*";
+        }
+        echo "<br>";
+      }
+      ?>
+    </div>
+    <div class="fig4">
+      <h4>Figura 4: Normal</h4>
+      <?php
+      for ($i=0; $i <= 2; $i++) {
+        echo "*";
+        for ($j=1; $j <= ($i*2); $j++) {
+          echo "*";
+        }
+        echo "<br>";
+      }
+      for ($i=2; $i >= 0; $i--) {
+        echo "*";
+        for ($j=4; $j >= ($i/2); $j--) {
+          echo "*";
+        }
+        echo "<br>";
+      }
+      ?>
+    </div>
+    <div class="fig5">
 
+    </div>
   </body>
 </html>
