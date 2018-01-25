65dd834eada0117665980f357d5ebaee0f9ba95e
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-01-25 10:44:48 +0100

    aassh

---
+++ php/baseDatos/insertarUsuario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Registro</title>
+    <link rel="stylesheet" href="registro.css">
+  </head>
+  <body>
+    <h1>CREAR PERFIL</h1>
+    <form action="#" method="post">
+      <fieldset class="caja">
+        <legend>¿QUIEN ERES?</legend>
+        <p>Nombre <br>
+        <input type="text" name="nombre" value="" placeholder="Introduce tu nombre"></p>
+        <p>Apellidos <br>
+          <input type="text" name="apellidos" value="" placeholder="Introduce tus apellidos"></p>
+        <p>Fecha de nacimiento: <br>
+        <input type="date" name="date"></p>
+        <p>Curso <br>
+          <input type="text" name="curso" value="" placeholder="Introduce tu curso"></p>
+      </fieldset>
+      <fieldset class="caja">
+        <legend>¿CÓMO QUIERES INICIAR SESIÓN?</legend>
+        <p>Nombre de usuario <br>
+          <input type="text" name="usuario" value="" placeholder="Introduce tu usuario"></p>
+        <p>Contraseña <br>
+          <input type="password" name="contrasenya" value="" placeholder="Introduce tu contraseña"></p>
+        <p>Repite la contraseña <br>
+          <input type="password" name="contrasenya2" value="" placeholder="Introduce tu contraseña"></p>
+      </fieldset>
+      <fieldset class="caja">
+        <legend>¿CONDICIONES DE REGISTRO?</legend>
+        <p>Me va a gustar este juego <br>
+        <input type="radio" name="gusto" value="">¡¡Por supuesto!!
+        <input type="radio" name="gusto" value="">Al principio no pero me voy a enganchar
+        <input type="radio" name="gusto" value="">Claro, es un clásico atemporal que [bla bla bla] y me va a encantar.</p>
+        <p><input type="checkbox" name="acuerdo" value="">Acepto jugar sin trampas e intentar mejorar dia a dia.</p>
+      </fieldset>
+      <input type="submit" name="crear" value="Crear cuenta" onclick="<?=crear()?>">
+    </form>
+    <button type="button" name="button" onclick="location.href='listadoUsuarios.php'">Volver</button>
+  </body>
+</html>
---
+++ php/baseDatos/listadoUsuarios.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Listado de Usuarios</title>
+    <link rel="stylesheet" href="listado.css">
+  </head>
+  <body>
+    <h1>BIENVENIDO AL AHORCADO</h1>
+    <br><br>
+    <div class="">
+      <!--formulario para elegir usuario existente-->
+      <fielset class="seleccionar">
+        <legend>Selecciona tu usuario.</legend>
+        <form class="user" action="ahorcado.php" method="post">
+          <select class="" name="">
+            <?php
+            $juegos=new mysqli("localhost","root","","juegos");
+            if ($juegos->connect_errno) {
+              echo "Fallo al conectar a MySQL: " . $juegos->connect_error;
+            } else {
+              ?>
+              <option value="">--Selecciona uno--</option>
+              <?php
+              //interactuar con la base de datos
+              $resultado = $juegos->query("select usuario from usuarios");
+              foreach ($resultado as $fila) {
+                ?>
+                <option value="<?=$fila['usuario']?>"><?=$fila['usuario']?></option>
+                <?php
+              }
+            }
+            ?>
+          </select>
+          <input type="submit" value="Empezar"></input>
+        </form>
+      </fielset>
+
+      <!--nuevo usuario-->
+      <fielset class="crear">
+        <legend>Crear nuevo usuario.</legend>
+        <button type="button" name="button" onclick="location.href='insertarUsuario.php'">Crear nuevo usuario</button>
+      </fielset>
+    </div>
+  </body>
+</html>
