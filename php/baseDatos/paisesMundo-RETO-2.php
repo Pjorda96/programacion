e16b31dff636302f1c788010c7d1c63a659eff98
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2018-01-26 19:43:16 +0100

    mods

--- php/baseDatos/paisesMundo-RETO-2.php
+++ php/baseDatos/paisesMundo-RETO-2.php
@@ -67,11 +67,24 @@
       } else {
         //interactuar con la base de datos
         $resultado = $world->query("select Name, Population from city join country on city.CountryCode=country.Code where country.Name like '%Jap%'");
-      ?>
-      <script type="text/javascript" src="script07-RETO-2.js"></script>
-      <?php
       }
       ?>
     </div>
+    <script type="text/javascript">
+    new Morris.Donut({
+      // ID of the element in which to draw the chart.
+      element: 'ciudadesJapon',
+      // Chart data records -- each entry in this array corresponds to a point on
+      // the chart.
+      data: [
+        <?php
+        foreach ($resultado as $fila) {
+          echo"{label: <?=$fila['Name']; ?>, value: <?=$fila['Population']; ?>},";
+        ?>
+        {label: "Valencia", value: 1}
+      ]
+    });
+
+    </script>
   </body>
 </html>
--- php/baseDatos/paisesMundo-RETO.php
+++ php/baseDatos/paisesMundo-RETO.php
@@ -66,11 +66,32 @@
       } else {
         //interactuar con la base de datos
         $resultado = $world->query("select Name, SurfaceArea from country order by SurfaceArea desc");
-      ?>
-      <script type="text/javascript" src="script07-RETO.js"></script>
-      <?php
       }
       ?>
     </div>
+    <script type="text/javascript">
+    new Morris.Bar({
+      // ID of the element in which to draw the chart.
+      element: 'paisesMundo',
+      // Chart data records -- each entry in this array corresponds to a point on
+      // the chart.
+      data: [
+        <?php
+        foreach ($resultado as $fila) {
+          echo"{ y: '<?=$fila['Name']; ?>', a: '<?=$fila['SurfaceArea']; ?>'},";
+        ?>
+        { y: 'hola', a: 100}
+      ],
+      // The name of the data record attribute that contains x-values.
+      xkey: <?=$fila['Name']; ?>,
+      // A list of names of data record attributes that contain y-values.
+      var a= <?=$world->query("select max(SurfaceArea) from country")?>;
+      var b= <?=$world->query("select min(SurfaceArea) from country")?>;
+      ykeys: [a,b],
+      // Labels for the ykeys -- will be displayed when you hover over the
+      // chart.
+      labels: ['Área']
+    });
+    </script>
   </body>
 </html>
