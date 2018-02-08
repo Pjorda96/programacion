1261e6964aea3af9a0d97a21807e454c8654c938
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-02-08 10:06:24 +0100

    yyr

--- php/baseDatos/perfilUsuario/actualizarUsuario-06.php
+++ php/baseDatos/perfilUsuario/actualizarUsuario-06.php
@@ -9,7 +9,16 @@
     <h1>MODIFICAR USUARIO</h1>
     <form class="" action="index.html" method="post">
       <fieldset>
-        
+        <?php
+        $juegos=new mysqli("localhost","root","","juegos");
+        if ($juegos->connect_errno) {
+          echo "Fallo al conectar a MySQL: " . $juegos->connect_error;
+        } else {
+          $borrar="delete from usuarios where usuario='".$_POST['usuario']."';";
+          $registro = $juegos->query($borrar);
+        }
+        ?>
+
       </fieldset>
     </form>
   </body>
