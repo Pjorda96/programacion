9ab4a22be015a2f7848a54a7cae3239702147c28
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-02-27 13:42:49 +0100

    figuritas

---
+++ php/figuritas08/Triangulo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ */
+class Triangulo
+{
+  //base y altura
+  private $base=0;
+  private $altura=0;
+
+  function __construct($base,$altura)
+  {
+    $this->base=$base;
+    $this->altura=$altura;
+  }
+}
