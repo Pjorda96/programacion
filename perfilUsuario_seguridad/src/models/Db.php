e6d70d04449664af4db86621913567640b1e27d7
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2018-04-21 12:31:24 +0200

    medio funciona, no saca la lista

--- perfilUsuario_seguridad/src/models/Db.php
+++ perfilUsuario_seguridad/src/models/Db.php
@@ -21,12 +21,6 @@ class Db
     private $pass="";
     private $db="";
 
-    private $usuario="";
-    private $nombre="";
-    private $apellidos="";
-    private $edad=0;
-    private $curso="";
-
     private $conector="";
 
     function __construct()
@@ -47,50 +41,7 @@ class Db
             $this->conector=$conectorTmp;
         }
     }
-    public function lista(){
-        $usuarios="select usuario from usuarios";
-        $resultado = $this->conector->query($usuarios);
-        foreach ($resultado as $fila) {
-            echo "<option value=";
-            echo $fila['usuario'];
-            echo ">";
-            echo $fila['usuario'];
-            echo "</option><br>";
-        }
-    }
 
-    public function leerDatos(){
-        $consulta="select nombre,apellidos,usuario,edad,curso from usuarios where usuario='".$_POST['usuario']."'";
-        //echo $consulta;
-        $consultaUsuario = $this->conector->query($consulta);
-        $this->usuario=$_POST['usuario'];
-
-        foreach ($consultaUsuario as $fila) {
-            $this->nombre=$fila['nombre'];
-            $this->apellidos=$fila['apellidos'];
-            $this->edad=$fila['edad'];
-            $this->curso=$fila['curso'];
-        }
-    }
-
-    public function insertarUsuario($nombre,$apellidos,$edad,$curso,$usuario,$contrasenya){
-        $insertar="insert into usuarios (nombre,apellidos,edad,curso,usuario,contrasenya) values
-    ('".$nombre."','".$apellidos."','".$edad."','".$curso."','".$usuario."','".$contrasenya."')";
-        //echo $insertar;
-        $registro = $this->conector->query($insertar);
-    }
-
-    public function actualizarUsuario($usuario,$nombre,$apellidos,$edad,$curso){
-        $actualizar="update usuarios set nombre='".$nombre."',apellidos='".
-            $apellidos."',edad='".$edad."',curso='".$curso."'
-    where usuario='".$usuario."';";
-        $registro = $this->conector->query($actualizar);
-    }
-
-    public function borrarUsuario($borrado){
-        $borrar="delete from usuarios where usuario='".$borrado."'";
-        $registro = $this->conector->query($borrar);
-    }
 
     public function getConector()
     {
@@ -127,45 +78,5 @@ class Db
     public function setDb($db)
     {
         $this->db = $db;
-    }
-    public function getUsuario()
-    {
-        return $this->usuario;
-    }
-    public function setUsuario($usuario)
-    {
-        $this->usuario = $usuario;
-    }
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-    }
-    public function getApellidos()
-    {
-        return $this->apellidos;
-    }
-    public function setApellidos($apellidos)
-    {
-        $this->apellidos = $apellidos;
-    }
-    public function getEdad()
-    {
-        return $this->edad;
-    }
-    public function setEdad($edad)
-    {
-        $this->edad = $edad;
-    }
-    public function getCurso()
-    {
-        return $this->curso;
-    }
-    public function setCurso($curso)
-    {
-        $this->curso = $curso;
     }
 }
--- perfilUsuario_seguridad/src/models/Usuario.php
+++ perfilUsuario_seguridad/src/models/Usuario.php
@@ -1,16 +1,118 @@
 <?php
 namespace Daw\models;
 
-use Daw\config\Db;
+use Daw\models\Db;
+use mysqli;
 /**
  *
  */
 class Usuario extends Db
 {
+    private $conector="";
+
+    private $usuario="";
+    private $nombre="";
+    private $apellidos="";
+    private $edad=0;
+    private $curso="";
+
     function __construct()
     {
         parent::conectar();
+        $this->setConector(parent::getConector());
     }
 
+    public function lista(){
+        $usuarios="select usuario from usuarios";
+        $resultado = $this->conector->query($usuarios);
+        foreach ($resultado as $fila) {
+            echo "<option value=";
+            echo $fila['usuario'];
+            echo ">";
+            echo $fila['usuario'];
+            echo "</option><br>";
+        }
+    }
 
+    public function leerDatos(){
+        $consulta="select nombre,apellidos,usuario,edad,curso from usuarios where usuario='".$_POST['usuario']."'";
+        //echo $consulta;
+        $consultaUsuario = $this->conector->query($consulta);
+        $this->usuario=$_POST['usuario'];
+
+        foreach ($consultaUsuario as $fila) {
+            $this->nombre=$fila['nombre'];
+            $this->apellidos=$fila['apellidos'];
+            $this->edad=$fila['edad'];
+            $this->curso=$fila['curso'];
+        }
+    }
+
+    public function insertarUsuario($nombre,$apellidos,$edad,$curso,$usuario,$contrasenya){
+        $insertar="insert into usuarios (nombre,apellidos,edad,curso,usuario,contrasenya) values
+    ('".$nombre."','".$apellidos."','".$edad."','".$curso."','".$usuario."','".$contrasenya."')";
+        //echo $insertar;
+        $registro = $this->conector->query($insertar);
+    }
+
+    public function actualizarUsuario($usuario,$nombre,$apellidos,$edad,$curso){
+        $actualizar="update usuarios set nombre='".$nombre."',apellidos='".
+            $apellidos."',edad='".$edad."',curso='".$curso."'
+    where usuario='".$usuario."';";
+        $registro = $this->conector->query($actualizar);
+    }
+
+    public function borrarUsuario($borrado){
+        $borrar="delete from usuarios where usuario='".$borrado."'";
+        $registro = $this->conector->query($borrar);
+    }
+
+    public function getConector()
+    {
+        return $this->conector;
+    }
+    public function setConector($conector)
+    {
+        $this->conector = $conector;
+    }
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+    }
+    public function getCurso()
+    {
+        return $this->curso;
+    }
+    public function setCurso($curso)
+    {
+        $this->curso = $curso;
+    }
 }
